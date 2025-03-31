16ba7dbd4b80c150149d83cd9e8c5f36f52550d6
hoowlr13 <n34677665@gmail.com>
2025-03-31 12:33:27 +0900

    25_03_31 저항항

---
+++ 저항.py
@@ -0,0 +1,5 @@
+color = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green':5, 'blue':6, 'violet': 7, 'grey':8, 'white':9}
+first = color[input()]
+second = color[input()]
+third = color[input()]
+print((first*10 + second) * (10 ** third))
